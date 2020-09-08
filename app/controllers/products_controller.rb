2960f2f23bd841976c59e2b58d4e673e8e060ce4
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-09-08 14:52:30 +0900

    レイアウト修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,11 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    if params[:name].present?
+      @products = Product.where('name LIKE ?', "%#{params[:name]}%")
+    else
+      @products = Product.all
+    end
   end
   
   def new
