f8730cb67fc2715a89854651d3bdf8e905f749b2
Tadakiyo-Hirano <d85nus03@gmail.com>
2020-09-09 17:35:00 +0900

    ログインフォームレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
+  # ログアウト用
+  # devise_scope :users do
+  #   delete :sign_out, to: 'devise/sessions#destroy', as: :destroy_user_session
+  # end
   
   root 'top_pages#top'
   resources :parts_lists, only: %i(index)
